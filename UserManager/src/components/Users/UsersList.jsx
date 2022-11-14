1bd62939cadac1430cc091174eb9b0689ffc9b5f
CarlozzBarry <seergejrachmaninov@gmail.com>
2022-11-14 11:48:09 +0100

    Update on styling

--- UserManager/src/components/Users/UsersList.jsx
+++ UserManager/src/components/Users/UsersList.jsx
@@ -1,6 +1,7 @@
 import styles from './UsersList.module.css'
 import React, {useState} from 'react';
 import User from './User.jsx';
+import Card from '../UI/Card';
 
 const UsersList = (props) => {
     
@@ -8,13 +9,15 @@ const UsersList = (props) => {
   if (props.users.length === 0) {
     return <h2 className='users-list__fallback'>Found no user</h2>
   }
-  return ( <ul>
-    {props.users.map0(user => (
+  return ( <Card>
+  <ul>
+    {props.users.map(user => (
         <li>
             {user.name} ({user.age} years old)
       </li>
     ))}
-  </ul>);
+  </ul>
+  </Card>);
   //The snippet above is sublime, fixes everything so to have like 2 lines in the return statement.
 };
 
