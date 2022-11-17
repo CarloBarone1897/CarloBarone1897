d35608eebbc0f3de658b60107db1e0e7816d5702
CarlozzBarry <seergejrachmaninov@gmail.com>
2022-11-17 11:33:50 +0100

    Update App.js

--- React_Projects/CredentialsManager/src/App.js
+++ React_Projects/CredentialsManager/src/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //We add then this function to check if there is a stored local value-key pair
 
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []); //I just moved into the useEffect all the functions before, the dependencies I'll add later
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false); //Here I'd like to store something
+  //Usually COOKIES and LOCAL STORAGES are used --> Here I'll go with LocalStorage.
+
+  //-->
+  localStorage.setItem("isLoggedIn", "1"); //Two strings, first one the variable, second one up to me for
+  //describing the state.
+  //Then, we want to check if this data is still in localStorage.
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
@@ -14,6 +28,7 @@ function App() {
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
