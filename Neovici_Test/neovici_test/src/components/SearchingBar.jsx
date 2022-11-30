ccf641a150b8c27e8ea3e54b70f8e3785ba6903b
CarlozzBarry <seergejrachmaninov@gmail.com>
2022-11-30 15:12:04 +0100

    Update SearchingBar.jsx

--- Neovici_Test/neovici_test/src/components/SearchingBar.jsx
+++ Neovici_Test/neovici_test/src/components/SearchingBar.jsx
@@ -2,7 +2,9 @@ import React, {useState} from "react";
 import './SearchingBar.css';
 
 const SearchingBar = (props) => {
-
+    return <form>
+        <input type='text'>Type your cocktail</input>
+    </form>
 }
 
 export default SearchingBar;
